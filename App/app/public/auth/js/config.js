859d5d47e15db919a286aeec08914dc133a30944
BenariDev02 <BenariDev02@DESKTOP-BPNS9HK>
2026-02-27 15:41:05 +0700

    ini perbaikhan favicon s

--- App/app/public/auth/js/config.js
+++
@@ -1,7 +0,0 @@
-const API_BASE =
-  window.location.hostname === "ashanum.com"
-    ? "https://ashanum.com"
-    : `http://${window.location.hostname}:9876`;
-
-window.API_BASE = API_BASE;
-console.log(API_BASE);
